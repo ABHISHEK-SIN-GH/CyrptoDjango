862f8b6b57448f82129f84a8d535f19ec0a184c4
Abhishek <singh.abhishek151019@gmail.com>
2022-05-04 23:16:10 +0530

    .

--- cyrptoAnalysis/cyrptoAnalysis/settings.py
+++ cyrptoAnalysis/cyrptoAnalysis/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-$eo-rykr%c!44mzmo0a(6e-#8@^5bwk+b#1#gk^+f$g9^-zzsn'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
